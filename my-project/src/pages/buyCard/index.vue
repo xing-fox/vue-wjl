d2952ec85f50bed2d1bb6236d2d4e5ee97a75bab
fox <450499752@qq.com>
2018-05-08 19:43:57 +0800

    start mpvue

---
+++ my-project/src/pages/buyCard/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  methods: {
+  },
+  created () {
+  }
+}
+</script>
+
+<style>
+</style>
+
---
+++ my-project/src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  methods: {
+  },
+  created () {
+  },
+  onLoad () {}
+}
+</script>
+
+<style lan="less" scoped>
+</style>
+
